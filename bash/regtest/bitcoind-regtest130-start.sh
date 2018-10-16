5a8ea2c6ff4da46cf2fcfe9de32e87874122a8eb
yinzhiqing <yinzhiqing@163.com>
2018-10-16 16:46:17 +0800

    1、modified bash file parameter ($* -> "$@") 2、modified CommitTransaction parameter(address -> account), wallet parse bug

--- bash/regtest/bitcoind-regtest130-start.sh
+++ bash/regtest/bitcoind-regtest130-start.sh
@@ -12,8 +12,8 @@ echo "启动bitcoind " $target
 echo "bitcoind path " $bitcoind_path
 cd $bitcoind_path
 
-echo ./bitcoind -regtest -port=$BITCOIND_PORT -rpcport=$BITCOIND_RPCPORT -bind=$node3 -rpcbind=$node3 -rpcallowip=$node3 -txindex=1 -datadir=${datadir_path} -conf=${bitcoin_conf} $BITCOIND_CMDS $*
-./bitcoind -regtest -port=$BITCOIND_PORT -rpcport=$BITCOIND_RPCPORT -bind=$node3 -rpcbind=$node3 -rpcallowip=$node3 -txindex=1 -datadir=${datadir_path} -conf=${bitcoin_conf} $BITCOIND_CMDS $*
+echo ./bitcoind -regtest -port=$BITCOIND_PORT -rpcport=$BITCOIND_RPCPORT -bind=$node3 -rpcbind=$node3 -rpcallowip=$node3 -txindex=1 -datadir=${datadir_path} -conf=${bitcoin_conf} $BITCOIND_CMDS "$@"
+./bitcoind -regtest -port=$BITCOIND_PORT -rpcport=$BITCOIND_RPCPORT -bind=$node3 -rpcbind=$node3 -rpcallowip=$node3 -txindex=1 -datadir=${datadir_path} -conf=${bitcoin_conf} $BITCOIND_CMDS "$@"
 
 
 cd -
