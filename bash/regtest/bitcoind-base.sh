166bb8e85201faae5878bd94f571c0a0cbeb1fab
yinzhiqing <yinzhiqing@163.com>
2018-10-19 16:21:18 +0800

    modified bash file , optimization...

--- bash/regtest/bitcoind-base.sh
+++ bash/regtest/bitcoind-base.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
-source ./nodes
+#addnode list
+source ./hosts
+#my defined funcs(show log functions)
+source ./log.sh
+
+localhost="127.0.0.1"
 user_home="/root/"
 bitcoind_path="/home/root/work/sinnga/src/"
 bitcoincli_path="/home/root/work/sinnga/src/"
 
-#regtest : 18407 main:8407
-BITCOIND_PORT=18407 
-BITCOIND_RPCPORT=18406
+#bitcoind bind and rpcbind setting
+##default port setting(regtest : 18407 main:8407)
+###main
+BITCOIND_PORT=8407       #bitcoind port
+BITCOIND_RPCPORT=18406    #bincoind rpc port
+###testnet
+BITCOIND_TESTPORT=8507       #bitcoind port
+BITCOIND_TESTRPCPORT=18506    #bincoind rpc port
+###regtest
+BITCOIND_REGPORT=8607       #bitcoind port
+BITCOIND_REGRPCPORT=18606    #bincoind rpc port
 
-localhost=""
+#default address setting
+target_addr="0.0.0.0"
+target_rpcaddr="127.0.0.1"
+
+#reset user user setting(configure file)
+source ./bind
+
+datadir_path=${user_home}".bitcoin/"
 
 BITCOIND_CMDS="-deprecatedrpc=accounts " #${ADDNODE_RPC_NODE1}" "${ADDNODE_RPC_NODE2}" "${ADDNODE_RPC_NODE3} 
+BITCOIND_TESTCMDS="-deprecatedrpc=accounts " #${ADDNODE_RPC_NODE1}" "${ADDNODE_RPC_NODE2}" "${ADDNODE_RPC_NODE3} 
+BITCOIND_REGCMDS="-deprecatedrpc=accounts " #${ADDNODE_RPC_NODE1}" "${ADDNODE_RPC_NODE2}" "${ADDNODE_RPC_NODE3} 
 
 find_index() { 
     x="${1%%$2*}"
@@ -39,14 +61,32 @@ while read line ; do
     node_name[$node_index]=${line:0:$pos}
     node_addr[$node_index]=$addr
     let node_index++
-done < nodes
+done < hosts
 
+#generate cmds
+#main
 for node in  ${node_addr[@]} 
 do
     addnode_cmds+=" -addnode="${node}":""${BITCOIND_PORT}"
 done
 
+##testnet
+for node in  ${node_addr[@]} 
+do
+    addnode_testcmds+=" -addnode="${node}":""${BITCOIND_TESTPORT}"
+done
+
+##regtest
+for node in  ${node_addr[@]} 
+do
+    addnode_regcmds+=" -addnode="${node}":""${BITCOIND_REGPORT}"
+done
+
 BITCOIND_CMDS+=$addnode_cmds
-echo $BITCOIND_CMDS
-echo ${node_name[@]}
-echo ${node_addr[@]}
+BITCOIND_TESTCMDS+=$addnode_testcmds
+BITCOIND_REGCMDS+=$addnode_regcmds
+logshow $BITCOIND_CMDS
+logshow $BITCOIND_TESTCMDS
+logshow $BITCOIND_REGCMDS
+logshow ${node_name[@]}
+logshow ${node_addr[@]}
