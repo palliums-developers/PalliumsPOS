b7eddb6ede1abd8d3963f57675593eef99ecb805
Bljszzl <zzl@palliums.org>
2018-11-20 19:00:49 +0800

    fengzhuang nodetooken Infterface

---
+++ src/omnicore/NodeToken.cpp
@@ -0,0 +1,6 @@
+#include "NodeToken.h"
+
+CNodeToken::CNodeToken()
+{
+
+}
---
+++ src/omnicore/rpcnt.h
@@ -0,0 +1,11 @@
+#ifndef RPCNT_H
+#define RPCNT_H
+
+#include <univalue.h>
+
+#include "rpc/server.h"
+
+
+UniValue sinnga_sendissuancefixed(const JSONRPCRequest& request);
+
+#endif // RPCNT_H
